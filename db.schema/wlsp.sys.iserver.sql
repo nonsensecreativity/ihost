fef77d12c56c1b1be677308a3ad0c2b6e50d7519
caiwang <caiwang@tsinghua.org.cn>
2014-12-06 20:21:16 +0800

    update wlsp.sys.iserver.sql

--- db.schema/wlsp.sys.iserver.sql
+++ db.schema/wlsp.sys.iserver.sql
@@ -5,11 +5,15 @@
 DROP TABLE IF EXISTS `ihostloc`;
 CREATE TABLE `authmac` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `mac` varchar(36) DEFAULT NULL,
-  `priip` varchar(64) DEFAULT NULL,
-  `pubip` varchar(64) DEFAULT NULL,
-  `apptype` varchar(36) DEFAULT NULL,
-  `location` varchar(64) DEFAULT NULL,
+  `mac` varchar(36) DEFAULT NULL, #mac address of eth0
+  `priip` varchar(64) DEFAULT NULL, #ip addr of eth0
+  `pubip` varchar(64) DEFAULT NULL, #ip addr retrived form request url
+  `rpttype` varchar(36) DEFAULT NULL, #status of this report:start;end;mid; of work
+  `apptype` varchar(36) DEFAULT NULL, #ma-meeting affair;hr-human resource;ms-mobile station
+  `dbinput` varchar(128) DEFAULT NULL, #db file imported into ihost at the begining (github url)
+  `dboutput` varchar(128) DEFAULT NULL, #db file dumped from ihost in the end (github url)
+  `location` varchar(64) DEFAULT NULL, #street name, building name or company name
+  `owner` varchar(64) DEFAULT NULL, #the owner of the ihost
   `latitude` varchar(36) DEFAULT NULL,
   `longitude` varchar(36) DEFAULT NULL,
   `rectime` datetime DEFAULT NULL,
