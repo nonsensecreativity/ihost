f5cd173b90bf9a9a2431089a2c81d10741ed44b8
caiwang <caiwang@tsinghua.org.cn>
2014-12-07 09:38:58 +0800

    admin pages for node info setting

--- home.admin/admin/sys/nodeget.php
+++ home.admin/admin/sys/nodeget.php
@@ -1,15 +1,8 @@
 <html>
-<title>cdns</title>
+<title>nodename</title>
 <body>
 <?php
-
-$str = "sudo  cat  /root/node  | grep nodename=";  
-//echo $str;
-$output = shell_exec($str);
-//echo "<pre>$output</pre>";
-$myArray = explode('=', $output);
-$nodename=$myArray[1];      
-
+ 
 $str = "sudo  ifconfig eth0 | grep 'eth0'  |  sed 's/ \{2,\}/ /g'";
 //echo $str;
 $output = shell_exec($str);
@@ -23,21 +16,144 @@ $output = shell_exec($str);
 //echo "<pre>$output</pre>";
 $myArray = explode(':', $output);
 $ipArray=explode(' ',$myArray[1]); 
-$ip=$ipArray[0]
+$ip=$ipArray[0];
 
+$str = "sudo  cat  /root/node  | grep rpttype=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$rpttype=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep apptype=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$apptype=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep dbinput=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$dbinput=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep dboutput=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$dboutput=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep location=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$location=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep company=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$company=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep owner=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$owner=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep latitude=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$latitude=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep longitude=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$longitude=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep admin=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$admin=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep contact=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$contact=$myArray[1];
+
+$str = "sudo  cat  /root/node  | grep memo=";
+//echo $str;
+$output = shell_exec($str);
+//echo "<pre>$output</pre>";
+$myArray = explode('=', $output);
+$memo=$myArray[1];
 ?> 
 
 <form action="nodeset.php" method="POST">  
     <label>MAC: <?php echo $mac; ?></label><br />
-    <input type="hidden" name="mac" size="20" value="<?php echo $mac; ?>"> 
+    <input type="hidden" name="mac" size="36" value="<?php echo $mac; ?>"> 
     <br /> 
     <label>IP: <?php echo $ip; ?></label><br />      
-    <input type="hidden" name="ip" size="20" value="<?php echo $ip; ?>">
-    <br />  
-    <label>Node name: </label><br />
-    <input type="text" name="nodename" size="32" value="<?php echo $nodename; ?>">
+    <input type="hidden" name="ip" size="64" value="<?php echo $ip; ?>">
     <br /> 
-    <input type="submit" value="set"/>  
+    <label>Report Type: </label><br />
+    <label>'start' for at the beginning; 'end' for at the end; 'mid' for at the middle</label><br />
+    <label>of a work procedure</label><br />
+    <input type="text" name="rpttype" size="36" value="<?php echo $rpttype; ?>">
+    <br />  
+    <label>Application Type: 'ma' for meeting affair; 'ms' for automobile station; ''</label><br />
+    <label>'ma' for meeting affair; 'ms' for automobile station; 'hr' for human resource</label><br />
+    <label>'other' for other application type</label><br />
+    <input type="text" name="apptype" size="36" value="<?php echo $apptype; ?>">
+    <br /> 
+	<label>Database file import at the beginning: (github url)</label><br />
+    <input type="text" name="dbinput" size="128" value="<?php echo $dbinput; ?>">
+    <br /> 
+    <label>Datebase file export at the end: (github url)</label><br />
+    <input type="text" name="dboutput" size="128" value="<?php echo $dboutput; ?>">
+    <br /> 
+    <label>Location: street name or building name </label><br />
+    <input type="text" name="location" size="64" value="<?php echo $location; ?>">
+    <br /> 
+    <label>Company: company name of the location</label><br />
+    <input type="text" name="company" size="64" value="<?php echo $company; ?>">
+    <br /> 
+    <label>Owner: owner of the ihost</label><br />
+    <input type="text" name="owner" size="64" value="<?php echo $owner; ?>">
+    <br /> 
+    <label>Latitude: latitude of the location</label><br />
+    <input type="text" name="latitude" size="36" value="<?php echo $latitude; ?>">
+    <br /> 
+    <label>Longitude: longitude of the location </label><br />
+    <input type="text" name="longitude" size="36" value="<?php echo $longitude; ?>">
+    <br /> 
+    <label>Administrator: administrator name of the ihost</label><br />
+    <input type="text" name="admin" size="36" value="<?php echo $admin; ?>">
+    <br /> 
+    <label>Contact: contact info of the administrator</label><br />
+    <input type="text" name="contact" size="64" value="<?php echo $contact; ?>">
+    <br /> 
+    <label>Memo: </label><br />
+    <input type="text" name="memo" size="128" value="<?php echo $memo; ?>">
+    <br /> 
+    <br />
+    <input type="submit" value="set"/>
+    
+          
 </form>  
 </body>
 
--- home.admin/admin/sys/nodeset.php
+++ home.admin/admin/sys/nodeset.php
@@ -7,14 +7,36 @@ $fname = "node";
 $orgpath = "/root/";
 $orgname = "node";
 
-$nodename = $_POST['nodename'];
 $mac = $_POST['mac'];   
-$ip = $_POST['ip'];   
+$ip = $_POST['ip']; 
+$rpttype = $_POST['rpttyep'];
+$apptype = $_POST['apptype'];
+$dbinput = $_POST['dbinput'];
+$dboutput = $_POST['dboutput'];
+$location = $_POST['location'];
+$company = $_POST['company'];
+$owner = $_POST['owner'];
+$latitude = $_POST['latitude'];
+$longitude = $_POST['longitude'];
+$admin = $_POST['admin'];
+$contact = $_POST['contact'];
+$memo = $_POST['memo'];
 
-$strline1 = "nodename="  . $nodename;
-$strline2 = "nodemac="  . $mac;
-$strline3 = "nodeip="  . $ip;
 
+$strline1 = "mac="  . $mac;
+$strline2 = "ip="  . $ip;
+$strline3 = "rpttype="  . $rpttype;
+$strline4 = "apptype="  . $apptype;
+$strline5 = "dbinput="  . $dbinput;
+$strline6 = "dboutput="  . $dboutput;
+$strline7 = "location="  . $location;
+$strline8 = "company="  . $company;
+$strline9 = "owner="  . $owner;
+$strline10 = "latitude="  . $latitude;
+$strline11 = "longitude="  . $longitude;
+$strline12 = "admin="  . $admin;
+$strline13 = "contact="  . $contact;
+$strline14 = "memo="  . $memo;
 
 $str = "sudo sed -i '1s/.*/" . $strline1  . "/' " . $orgpath . $orgname;
 //echo $str;
@@ -31,31 +53,88 @@ $str = "sudo sed -i '3s/.*/" . $strline3  . "/' " . $orgpath . $orgname;
 $output = shell_exec($str);
 //echo "<br />";
 
+$str = "sudo sed -i '4s/.*/" . $strline4  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+
+$str = "sudo sed -i '5s/.*/" . $strline5  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+$str = "sudo sed -i '6s/.*/" . $strline6  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+$str = "sudo sed -i '7s/.*/" . $strline7  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+$str = "sudo sed -i '8s/.*/" . $strline8  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+$str = "sudo sed -i '9s/.*/" . $strline9  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+$str = "sudo sed -i '10s/.*/" . $strline10  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+$str = "sudo sed -i '11s/.*/" . $strline11  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+$str = "sudo sed -i '12s/.*/" . $strline12  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+$str = "sudo sed -i '13s/.*/" . $strline13  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+$str = "sudo sed -i '14s/.*/" . $strline14  . "/' " . $orgpath . $orgname;
+//echo $str;
+$output = shell_exec($str);
+//echo "<br />";
+
 echo "<br />";
-echo "Done! <br />";
+echo "Local Done! <br />";
 
-$str = "sudo  head -1 " . $orgpath . $orgname . " | tail -1";
+$str = "sudo  cat  " . $orgpath . $orgname ;
 $output = shell_exec($str);
-//echo "<pre>$output</pre>";
-$myArray = explode('=', $output);
-$name = str_replace("\n","",$myArray[1]);
+echo "<pre>$output</pre>";
 
-$str = "sudo  head -2 " . $orgpath . $orgname . " | tail -1";
-$output = shell_exec($str);
-//echo "<pre>$output</pre>";
-$myArray = explode('=', $output);
-$mac = str_replace("\n","",$myArray[1]);
+/*
+// Push ihost node info to iserver
+echo "<br />Push to iserver... <br />";
+$url = "http://182.92.195.40:8080/";
+$url = $url . "face-detection/detection/123/";
 
-$str = "sudo  head -3 " . $orgpath . $orgname . " | tail -1";
-$output = shell_exec($str);
-//echo "<pre>$output</pre>";
-$myArray = explode('=', $output);
-$ip = str_replace("\n","",$myArray[1]);
+// Initialize the cURL session with the request URL
+$session = curl_init($url);
+//echo $session;
 
-echo "nodename: " . $name . "<br />"; 
-echo "nodemac: " . $mac . "<br />"; 
-echo "nodeip: " . $ip . "<br />"; 
+// Tell cURL to return the request data
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 
+// Set the HTTP request authentication headers
+$headers = array("content-type":"application/json;charset=UTF-8");
+print $headers;
+curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
+
+// Execute cURL on the session handle
+$response = curl_exec($session);
+
+// Close the cURL session
+curl_close($session);
+
+echo $response . "<br />";
+echo "<br />";
+echo "Remote Done! <br />";
+
+*/
 ?> 
 </body>
 </html>
