48a339c016d36acb33f1e7c7ae7fff5d19965799
caiwang <caiwang@tsinghua.org.cn>
2014-12-07 12:37:42 +0800

    change column 'sender' to 'progid'

--- home.admin/admin/sys/nodeget.php
+++ home.admin/admin/sys/nodeget.php
@@ -104,50 +104,49 @@ $memo=$myArray[1];
 ?> 
 
 <form action="nodeset.php" method="POST">  
-    <label>MAC: <?php echo $mac; ?></label><br />
-    <input type="hidden" name="mac" size="36" value="<?php echo $mac; ?>"> 
-    <br /> 
-    <label>IP: <?php echo $ip; ?></label><br />      
+    <label style="font-weight:bold;font-size:12px;">MAC: <?php echo $mac; ?></label><br />
+    <input type="hidden" name="mac" size="36" value="<?php echo $mac; ?>">  
+    <label style="font-weight:bold;font-size:12px;">IP: <?php echo $ip; ?></label><br />      
     <input type="hidden" name="ip" size="64" value="<?php echo $ip; ?>">
     <br /> 
-    <label>Report Type: </label><br />
-    <label>'start' for at the beginning; 'end' for at the end; 'mid' for at the middle</label><br />
-    <label>of a work procedure</label><br />
+    <label style="font-weight:bold;font-size:12px;">Report Type: </label><br />
+    <label style="font-size:12px;">'start' for at the beginning; 'end' for at the end; 'mid' for at the middle</label><br />
+    <label style="font-size:12px;">of a work procedure</label><br />
     <input type="text" name="rpttype" size="36" value="<?php echo $rpttype; ?>">
-    <br />  
-    <label>Application Type: 'ma' for meeting affair; 'ms' for automobile station; ''</label><br />
-    <label>'ma' for meeting affair; 'ms' for automobile station; 'hr' for human resource</label><br />
-    <label>'other' for other application type</label><br />
+    <br /> 
+    <label style="font-weight:bold;font-size:12px;">Application Type: 'ma' for meeting affair; 'ms' for automobile station; ''</label><br />
+    <label style="font-size:12px;">'ma' for meeting affair; 'ms' for automobile station; 'hr' for human resource</label><br />
+    <label style="font-size:12px;">'other' for other application type</label><br />
     <input type="text" name="apptype" size="36" value="<?php echo $apptype; ?>">
-    <br /> 
-	<label>Database file import at the beginning: (github url)</label><br />
+    <br />
+	<label style="font-weight:bold;font-size:12px;">Database file import at the beginning: (github url)</label><br />
     <input type="text" name="dbinput" size="128" value="<?php echo $dbinput; ?>">
-    <br /> 
-    <label>Datebase file export at the end: (github url)</label><br />
+    <br />
+    <label style="font-weight:bold;font-size:12px;">Datebase file export at the end: (github url)</label><br />
     <input type="text" name="dboutput" size="128" value="<?php echo $dboutput; ?>">
-    <br /> 
-    <label>Location: street name or building name </label><br />
+    <br />
+    <label style="font-weight:bold;font-size:12px;">Location: street name or building name </label><br />
     <input type="text" name="location" size="64" value="<?php echo $location; ?>">
     <br /> 
-    <label>Company: company name of the location</label><br />
+    <label style="font-weight:bold;font-size:12px;">Company: company name of the location</label><br />
     <input type="text" name="company" size="64" value="<?php echo $company; ?>">
-    <br /> 
-    <label>Owner: owner of the ihost</label><br />
+    <br />
+    <label style="font-weight:bold;font-size:12px;">Owner: owner of the ihost</label><br />
     <input type="text" name="owner" size="64" value="<?php echo $owner; ?>">
-    <br /> 
-    <label>Latitude: latitude of the location</label><br />
+    <br />
+    <label style="font-weight:bold;font-size:12px;">Latitude: latitude of the location</label><br />
     <input type="text" name="latitude" size="36" value="<?php echo $latitude; ?>">
-    <br /> 
-    <label>Longitude: longitude of the location </label><br />
+    <br />
+    <label style="font-weight:bold;font-size:12px;">Longitude: longitude of the location </label><br />
     <input type="text" name="longitude" size="36" value="<?php echo $longitude; ?>">
-    <br /> 
-    <label>Administrator: administrator name of the ihost</label><br />
+    <br />
+    <label style="font-weight:bold;font-size:12px;">Administrator: administrator name of the ihost</label><br />
     <input type="text" name="admin" size="36" value="<?php echo $admin; ?>">
-    <br /> 
-    <label>Contact: contact info of the administrator</label><br />
+    <br />
+    <label style="font-weight:bold;font-size:12px;">Contact: contact info of the administrator</label><br />
     <input type="text" name="contact" size="64" value="<?php echo $contact; ?>">
     <br /> 
-    <label>Memo: </label><br />
+    <label style="font-weight:bold;font-size:12px;">Memo: </label><br />
     <input type="text" name="memo" size="128" value="<?php echo $memo; ?>">
     <br /> 
     <br />
