770a8b2cad3fe901b355215827ba6cc218604122
michaely <gt8442b@yahoo.com>
2014-12-07 20:49:26 -0500

    uses set type for rpttype and apptype.
     add comments.

--- db.schema/wlsp.sys.iserver.sql
+++ db.schema/wlsp.sys.iserver.sql
@@ -5,11 +5,11 @@
 DROP TABLE IF EXISTS `ihostloc`;
 CREATE TABLE `ihostloc` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `mac` varchar(36) DEFAULT NULL, #mac address of eth0
-  `priip` varchar(64) DEFAULT NULL, #ip addr of eth0
-  `pubip` varchar(64) DEFAULT NULL, #ip addr retrived form request url
-  `rpttype` varchar(36) DEFAULT NULL, #status of this report:start;end;mid; of work
-  `apptype` varchar(36) DEFAULT NULL, #ma-meeting affair;hr-human resource;ms-mobile station
+  `mac` varchar(36) DEFAULT NULL, #mac address of eth0 on the ihost
+  `priip` varchar(64) DEFAULT NULL, #private ip addr of eth0
+  `pubip` varchar(64) DEFAULT NULL, #public ip addr retrived form request url
+  `rpttype` set('start', 'end', 'mid') not NULL default 'start', #status of this report:start;end;mid; of work
+  `apptype` set('ma', 'hr', 'ms') not NULL default 'ma', #ma-meeting affair;hr-human resource;ms-mobile station
   `dbinput` varchar(128) DEFAULT NULL, #db file imported into ihost at the begining (github url)
   `dboutput` varchar(128) DEFAULT NULL, #db file dumped from ihost in the end (github url)
   `city` varchar(64) DEFAULT NULL, #city name
@@ -25,5 +25,5 @@ CREATE TABLE `ihostloc` (
   `progid` varchar(36) DEFAULT NULL,
   `rectime` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `mac` (`mac`)
+  KEY `mac` (`mac`)   #mac address of the ihost
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
