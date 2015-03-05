c571456a0eddf290d48860c229481c03a353d973
michaelyin <gt8442b@yahoo.com>
2015-03-05 00:22:42 -0500

    initial version. TODO: add relationship

--- db.schema/wlsp.sys.base.sql
+++ db.schema/wlsp.sys.base.sql
@@ -18,59 +18,59 @@
 ## ----------------------------
 DROP TABLE IF EXISTS `useraccounts`;	#	用户主记录
 CREATE TABLE `useraccounts` (	#	
-  `id` bigint NOT NULL AUTO_INCREMENT,	#	convert to uuid.
-  `userid` varchar(36) DEFAULT NULL,	#	为user分配一个uuid. It is generated in ihost by php. remove
-  `srcid` int DEFAULT NULL,	            #	iserver字段 remove
-  `token` int DEFAULT NULL,	            #	
-  `srcnode` varchar(10) DEFAULT NULL,	#	（预留） remove
-  `usercode` varchar(30) DEFAULT NULL,	#	用户编码（预留） remove
-  `user_uuid` varchar(36) DEFAULT NULL,	#	用户uuid. It is generated on iserver. Globally effective to cover cases that the user might change his phone andor phone number. remove
-  `mac` varchar(36) DEFAULT NULL,	    #	mac地址 remove, use phone number to identify user
-  `userpass` varchar(30) DEFAULT NULL,	#	用户密码 //remove
-  `useremail1` varchar(64) DEFAULT NULL,	#	用户email //move to a separate table, such as user_info
-  `useremail2` varchar(64) DEFAULT NULL,	#	用户备用email //remove
-  `question` varchar(30) DEFAULT NULL,	#	密码提示问题 //remove
-  `answer` varchar(30) DEFAULT NULL,	#	密码答案，用于找回密码 // remove, use user activity to verify user.
-  `fname` varchar(20) DEFAULT NULL,	#	名字 
-  `lname` varchar(20) DEFAULT NULL,	#	姓   
-  `userrole` varchar(30) default '0', #	不同角色，每个mac在每个userrole中有一个default userid #   100-代表 200-嘉宾 300-媒体 400-会务 //???
-  `usertype` varchar(10) DEFAULT NULL,	#	用户类型：预注册/现场注册 ??
-  `integral` int DEFAULT '0',	#	userid下的积分 ?? better name
-  `pntfactor` int DEFAULT '1000',	#	points转integral的因子，1000代表1 // put it together with integral in a separate table
-  `byear` smallint DEFAULT NULL,	#	生日，年 // move to user_info. It can be obtained in different ways.
-  `bmonth` smallint DEFAULT NULL,	#	生日，月
-  `bday` smallint DEFAULT NULL,	    #	生日，日
-  `gender` varchar(8) DEFAULT NULL,	#	性别  //user info.
-  `occup` varchar(30) DEFAULT NULL,	#	职业 // user info
-  `orgn` varchar(64) DEFAULT NULL,	#	工作单位 //user info
-  `title` varchar(32) DEFAULT NULL,	#	职务 // user info.
-  `cid` varchar(30) DEFAULT '000000',	#	证件号, cid + ctype
-  `ctype` varchar(10) DEFAULT NULL,	#	证件类别 //remove.
-  `regphone` varchar(30) DEFAULT NULL,	#	（预）注册所用的电话号码 
-  `captcha` varchar(10) DEFAULT NULL,	#	（预）注册所用的验证码
-  `phone` varchar(30) DEFAULT NULL,	#	常用电话号码 , phone number for receiving sms.
-  `backphone` varchar(30) DEFAULT NULL,	#	备用电话号码 //remove
-  `address` varchar(128) DEFAULT NULL,	#	地址 // to memo
-  `location` varchar(32) DEFAULT NULL,	#	所在区域 //to memo
-  `action` varchar(128) DEFAULT NULL,	#	活动（预留） //remove
+ 
   `stat` varchar(3) DEFAULT '100',	#	数据状态 100-有效  //change it to enum
-  `open1` varchar(3) DEFAULT '100',	#	数据对招聘者公开，100-公开，0-不公开 //remove
-  `open2` varchar(3) DEFAULT '100',	#	数据对求职者公开，100-公开，0-不公开 //remove
-  `smscheck` varchar(3) DEFAULT '100',	#	短信验证，100-验证，0-不验证  // flag to verify user.
+ 
   `memo` varchar(128) DEFAULT NULL,	#	备注//different table for unstructured info.
-  `srcip` varchar(64) DEFAULT NULL,	#	iserver字段 //remove
-  `sender` varchar(36) DEFAULT NULL, 	#	iserver字段 //remove
-  `netid` varchar(36) DEFAULT NULL, 	#	iserver字段 //remove
-  `progid` varchar(36) DEFAULT NULL, 	#	iserver字段 //remove
-  `intid` varchar(30) DEFAULT NULL,	#	与phpyun的对应关系（记录学历等其他个人信息） //remove
-  `modify_t` datetime DEFAULT NULL,	#	记录更新时间
-  `create_t` datetime DEFAULT NULL,	#	记录时间
-  `pushflag` smallint  unsigned DEFAULT '1', // ?? flag for syn with iserver
+  
   PRIMARY KEY (`id`),	#	
   KEY `userid` (`userid`),	#	
   KEY `phone` (`phone`), #	
   KEY `usercode` (`usercode`) //remove
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;	#	
+
+# one phone number mapping to an account, but it could be mapped to multiple mac
+# token table
+# http://www.onurguzel.com/storing-mac-address-in-a-mysql-database/
+
+DROP TABLE IF EXISTS `token`;
+CREATE TABLE  `token` (
+  phone varchar(30) NOT NULL,
+  mac BIGINT UNSIGNED NOT NULL,
+  token int UNSIGNED NOT NULL
+)CHARSET=utf8;
+
+
+
+DROP TABLE IF EXISTS user_info;
+CREATE TABLE user_info (
+    id VARCHAR(36) NOT NULL,
+    first_name` varchar(20) DEFAULT NULL,	#	名字 
+    last_name` varchar(20) DEFAULT NULL,	#	姓   
+    email varchar(64) NOT NULL DEFAULT '',
+    byear smallint DEFAULT NULL,	#	生日，年 // move to user_info. It can be obtained in different ways.
+    bmonth smallint DEFAULT NULL,	#	生日，月
+    bday smallint DEFAULT NULL,	    #	生日，日
+    gender varchar(8) DEFAULT NULL,	#	性别  //user info.
+    occup varchar(30) DEFAULT NULL,	#	职业 // user info
+    orgn varchar(64) DEFAULT NULL,	#	工作单位 //user info
+    title varchar(32) DEFAULT NULL,	#	职务 // user info.
+    cid varchar(30) DEFAULT '000000',	#	证件号, cid + ctype
+    ctype varchar(10) DEFAULT NULL,	#	证件类别 //remove.
+    address varchar(128) DEFAULT NULL,	#	地址 // to memo
+    location varchar(32) DEFAULT NULL,	#	所在区域 //to memo
+    
+)
+DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE account_point (
+   mac BIGINT UNSIGNED NOT NULL,
+   point int not null DEFAULT '0',	#	userid下的积分 ?? better name
+   factor int DEFAULT '1000',	#	points转integral的因子，1000代表1 // put it together with integral in a separate table
+
+) DEFAULT CHARSET=utf8;
 
 ###################################   account table ###########################################################################################
 ##1，account base on ihost
@@ -79,12 +79,12 @@ CREATE TABLE `useraccounts` (	#
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE account (
     `id` VARCHAR(36) NOT NULL,                          ##id int unsigned NOT NULL auto_increment primary key, the same user can have differennt account at different ihost
-    `mac` varchar(64) DEFAULT NULL,
-    password varchar(50) NOT NULL,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL,
-    email varchar(50) NOT NULL,
-    enabled boolean NOT NULL,
+    phone varchar(30) DEFAULT NULL,	#	常用电话号码 , mobile phone number for receiving sms.
+    sent_to_server boolean NOT NULL DEFAULT 0, // ?? flag for syn with iserver
+    smscheck boolean NOT NULL default 0,
+    enabled boolean NOT NULL default 0,
+    create_t datetime DEFAULT NULL,	#	记录时间
+    modify_t datetime DEFAULT NULL,	#	记录更新时间
     unique index account_idx1 (mac)
 ) DEFAULT CHARSET=utf8;
 
